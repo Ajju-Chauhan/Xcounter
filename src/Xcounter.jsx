733fce3075c0eaf6287a603610d0b76ee1d5420e
Ajju-Chauhan <142608291+Ajju-Chauhan@users.noreply.github.com>
2024-06-08 11:29:14 +0530

    made changes on setCounter()

--- src/Xcounter.jsx
+++ src/Xcounter.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
 
-
 function Xcounter() {
-    let [counter , setCounter] = useState(0)
+    // Initialize the counter state with an initial value of 0
+    let [counter, setCounter] = useState(0);
+
+    // Function to increment the counter value by 1
     function increment() {
         setCounter(counter + 1);
     }
 
+    // Function to decrement the counter value by 1
     function decrement() {
         setCounter(counter - 1);
     }
@@ -14,12 +17,11 @@ function Xcounter() {
     return (
         <div>
             <h1>Counter App</h1>
-            <p>Count : {counter}</p>
+            <p>Count: {counter}</p>
             <button onClick={increment}>Increment</button>
             <button onClick={decrement}>Decrement</button>
         </div>
-    )
-    
+    );
 }
 
 export default Xcounter;
